42727099f54b4f8917b78c0fdda848d0b072fe47
Omprakash Gautam <omprakash201194@gmail.com>
2021-06-13 11:08:18 +0530

    React app with Team page

--- src/main/java/com/omprakashgautam/ipldashboard/controller/TeamController.java
+++ src/main/java/com/omprakashgautam/ipldashboard/controller/TeamController.java
@@ -6,6 +6,7 @@ import com.omprakashgautam.ipldashboard.repository.MatchRepository;
 import com.omprakashgautam.ipldashboard.repository.TeamRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import java.util.Optional;
  */
 @RestController
 @Slf4j
+@CrossOrigin
 public class TeamController {
 
     private final TeamRepository teamRepository;
