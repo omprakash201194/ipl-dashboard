6713ef6a415193ce5ad4d9e70bbd80ce457a186a
Omprakash Gautam <omprakash201194@gmail.com>
2021-06-13 12:25:11 +0530

    Adding react routes and links to team name

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,30 +1,31 @@
-import {React, useEffect, useState} from 'react';
-import {MatchDetailCard} from '../components/MatchDetailCard'
-import {MatchSmallCard} from '../components/MatchSmallCard'
-
+import { React, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { MatchDetailCard } from "../components/MatchDetailCard";
+import { MatchSmallCard } from "../components/MatchSmallCard";
 
 export const TeamPage = () => {
+  const [team, setTeam] = useState({ matches: [] });
+  const { teamName } = useParams();
+  useEffect(() => {
+    const fetchTeam = async () => {
+      const response = await fetch(`http://localhost:8080/team/${teamName}`);
+      const data = await response.json();
+      setTeam(data);
+    };
+    fetchTeam();
+  }, [teamName]);
 
-    const [team, setTeam] = useState({matches: []});
-      useEffect(
-          () => {
-          const fetchTeam = async () => {
-              const response = await fetch('http://localhost:8080/team/Rajasthan Royals');
-              const data = await response.json();
-              setTeam(data);
-
-          };
-          fetchTeam();
-          },[]
-      );
-
+  if (!team || !team.teamName) {
+    return <h1>Team not Found</h1>;
+  }
   return (
     <div className="TeamPage">
       <h1>{team.teamName}</h1>
-      <MatchDetailCard match={team.matches[0]}/>
-      {team.matches.slice(1).map(match => <MatchSmallCard match={match}/>)}
+      <MatchDetailCard teamName = {team.teamName} match={team.matches[0]} />
+      {team.matches.slice(1).map((match) => (
+        <MatchSmallCard teamName = {team.teamName} match={match} />
+      ))}
     </div>
   );
-
-}
+};
 export default TeamPage;
